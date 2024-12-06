aa264cd497e75cf645daa77c81d8a19d718531b8
Konrad Heidler <k.heidler@tum.de>
2024-12-06 16:33:52 +0000

    Go for stable training

--- lib/ddpm_trainer.py
+++ lib/ddpm_trainer.py
@@ -20,7 +20,7 @@ class DDPMTrainer:
     model_rngs = nnx.Rngs(init_key)
     model = COBRA(config.model, rngs=model_rngs)
     model.train()
-    opt = optax.chain(optax.clip(1.0), optax.adam(1e-3))
+    opt = optax.chain(optax.clip(1.0), optax.adam(1e-4, b1=0.9, b2=0.99))
     self.state = nnx.Optimizer(model, opt)
 
     self.loss_fn = getattr(losses, config.loss_function)()
