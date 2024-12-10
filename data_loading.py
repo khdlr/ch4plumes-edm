3d279e5c6568172c20e2646e27b64f519b41ba09
Konrad Heidler <k.heidler@tum.de>
2024-12-10 13:19:53 +0000

    Smaller epochs

--- data_loading.py
+++ data_loading.py
@@ -4,7 +4,7 @@ import tensorflow_datasets as tfds
 def get_loader(batch_size, mode):
   ds = tfds.load("zakynthos/all", split=mode)
   if mode == "train":
-    ds = ds.repeat(100)
+    ds = ds.repeat(10)
     ds = ds.shuffle(1024)
   ds = ds.batch(batch_size)
 
