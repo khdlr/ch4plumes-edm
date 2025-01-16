df2e333bec0fc6dfbfeef446e7bf17bb79a661bd
Konrad Heidler <k.heidler@tum.de>
2025-01-16 08:46:19 +0000

    Coastlines from Mask

--- data_loading.py
+++ data_loading.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import tensorflow_datasets as tfds
 
 
@@ -7,14 +8,29 @@ def pseudo_dem(batch):
   return batch
 
 
+def prepare_coastlines(batch):
+  batch = dict(**batch)
+  m = batch["mask"]
+  batch["image"] = 255 * tf.cast(tf.stack([m, m, m], axis=-1), tf.uint8)
+  batch["dem"] = 300 * tf.cast(tf.stack([m], axis=-1), tf.float32)
+  batch["contour"] = tf.reverse(batch["contour"], axis=[-1])
+  batch["filename"] = tf.strings.reduce_join(
+    tf.strings.as_string(batch["xyz"]), separator="/", axis=-1
+  )
+  return batch
+
+
 def get_loader(batch_size, mode):
-  name = "zakynthos"
+  name = "coastlines"
   ds = tfds.load(name, split=mode)
   if mode == "train":
-    ds = ds.repeat(50)
+    if name == "zakynthos":
+      ds = ds.repeat(50)
     ds = ds.shuffle(1024)
   ds = ds.batch(batch_size)
   if name == "synthetic_contours":
     ds = ds.map(pseudo_dem)
+  elif name == "coastlines":
+    ds = ds.map(prepare_coastlines)
 
   return tfds.as_numpy(ds)
