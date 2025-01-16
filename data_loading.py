Fix mask scaling for coastlines

data_loading.py
@@ -11,8 +11,8 @@ def pseudo_dem(batch):
 def prepare_coastlines(batch):
   batch = dict(**batch)
   m = batch["mask"]
-  batch["image"] = 255 * tf.cast(tf.concat([m, m, m], axis=-1), tf.uint8)
-  batch["dem"] = 300 * tf.cast(m, tf.float32)
+  batch["image"] = tf.cast(tf.concat([m, m, m], axis=-1), tf.uint8)
+  batch["dem"] = tf.cast(m, tf.float32)
   batch["contour"] = tf.reverse(batch["contour"], axis=[-1])
   batch["filename"] = tf.strings.reduce_join(
     tf.strings.as_string(batch["xyz"]), separator="/", axis=-1